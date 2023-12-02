d0e10d561d7766c0d30c4dfa75b205561918d0de
HenryDev10 <117412713+HenryDev10@users.noreply.github.com>
2023-12-02 13:33:22 -0500

    Add files via upload

--- RegistroDeAsistencia/src/main/java/com/utp/registrodeasistencia/view/GestionDeUsuarios.java
+++ RegistroDeAsistencia/src/main/java/com/utp/registrodeasistencia/view/GestionDeUsuarios.java
@@ -4,12 +4,18 @@
  */
 package com.utp.registrodeasistencia.view;
 
+import com.utp.registrodeasistencia.controller.ConnectionDB;
 import com.utp.registrodeasistencia.controller.UsuarioDaoImpl;
 import com.utp.registrodeasistencia.model.Horario;
 import com.utp.registrodeasistencia.model.Usuario;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -39,16 +45,16 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        tblUsuarios = new javax.swing.JTable();
+        TablaUsuarios = new javax.swing.JTable();
         txtNombre = new javax.swing.JTextField();
         txtApellido = new javax.swing.JTextField();
         txtCorreo = new javax.swing.JTextField();
         txtContrasenia = new javax.swing.JTextField();
         cbxRol = new javax.swing.JComboBox<>();
-        btnGuardar = new javax.swing.JButton();
+        GuardarUsuario = new javax.swing.JButton();
         btnModificar = new javax.swing.JButton();
         btnDesactivar = new javax.swing.JButton();
-        btnLimpiar = new javax.swing.JButton();
+        Limpiarcampo = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
         cbxActivo = new javax.swing.JComboBox<>();
         jLabel6 = new javax.swing.JLabel();
@@ -71,7 +77,7 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
         jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
         jLabel4.setText("Rol:");
 
-        tblUsuarios.setModel(new javax.swing.table.DefaultTableModel(
+        TablaUsuarios.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -82,7 +88,7 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(tblUsuarios);
+        jScrollPane1.setViewportView(TablaUsuarios);
 
         txtNombre.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         txtNombre.setPreferredSize(new java.awt.Dimension(59, 27));
@@ -100,13 +106,13 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
         cbxRol.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Colaborador", "Administrador" }));
         cbxRol.setPreferredSize(new java.awt.Dimension(56, 27));
 
-        btnGuardar.setBackground(new java.awt.Color(15, 118, 210));
-        btnGuardar.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        btnGuardar.setForeground(new java.awt.Color(255, 255, 255));
-        btnGuardar.setText("Guardar");
-        btnGuardar.addActionListener(new java.awt.event.ActionListener() {
+        GuardarUsuario.setBackground(new java.awt.Color(15, 118, 210));
+        GuardarUsuario.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        GuardarUsuario.setForeground(new java.awt.Color(255, 255, 255));
+        GuardarUsuario.setText("Guardar");
+        GuardarUsuario.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnGuardarActionPerformed(evt);
+                GuardarUsuarioActionPerformed(evt);
             }
         });
 
@@ -120,10 +126,15 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
         btnDesactivar.setForeground(new java.awt.Color(255, 255, 255));
         btnDesactivar.setText("Desactivar");
 
-        btnLimpiar.setBackground(new java.awt.Color(15, 118, 210));
-        btnLimpiar.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        btnLimpiar.setForeground(new java.awt.Color(255, 255, 255));
-        btnLimpiar.setText("Limpiar");
+        Limpiarcampo.setBackground(new java.awt.Color(15, 118, 210));
+        Limpiarcampo.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        Limpiarcampo.setForeground(new java.awt.Color(255, 255, 255));
+        Limpiarcampo.setText("Limpiar");
+        Limpiarcampo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                LimpiarcampoActionPerformed(evt);
+            }
+        });
 
         jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
         jLabel5.setText("Estado:");
@@ -177,13 +188,13 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(btnGuardar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                                .addComponent(GuardarUsuario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel6)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(txtContrasenia, javax.swing.GroupLayout.PREFERRED_SIZE, 380, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnLimpiar, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE))
+                        .addComponent(Limpiarcampo, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
@@ -212,7 +223,7 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnGuardar))
+                    .addComponent(GuardarUsuario))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -226,7 +237,7 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
                     .addComponent(btnDesactivar))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnLimpiar)
+                    .addComponent(Limpiarcampo)
                     .addComponent(jLabel6)
                     .addComponent(txtContrasenia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -258,7 +269,7 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
+    private void GuardarUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GuardarUsuarioActionPerformed
        
         Usuario usuario2 = new Usuario();
         
@@ -268,24 +279,62 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
         usuario2.setNombre(txtNombre.getText());
         usuario2.setApellido(txtApellido.getText());
         usuario2.setCorreo(txtCorreo.getText());
-        usuario2.setRol("COLABORADOR");
-        
-        
+        usuario2.setRol(cbxRol.getSelectedItem().toString());
+                
         UsuarioDaoImpl dao = new UsuarioDaoImpl();
         try {
             dao.registrar(usuario2);
-            JOptionPane.showMessageDialog(null, "Usuario Creado", "Usuario Creado", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Usuario Registrado", "Usuario Creado", JOptionPane.INFORMATION_MESSAGE);
         } catch (Exception ex) {
             Logger.getLogger(GestionDeUsuarios.class.getName()).log(Level.SEVERE, "Error en el metodo Registrar Usuario", ex);
         }
-    }//GEN-LAST:event_btnGuardarActionPerformed
+    }//GEN-LAST:event_GuardarUsuarioActionPerformed
 
-    
+    private void LimpiarcampoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LimpiarcampoActionPerformed
+        txtDni.setText("");
+        cbHorario.setSelectedIndex(0);
+        txtContrasenia.setText("");
+        txtNombre.setText("");
+        txtApellido.setText("");
+        txtCorreo.setText("");
+        cbxRol.setSelectedIndex(0);
+    }//GEN-LAST:event_LimpiarcampoActionPerformed
+
+    private void TablaUsuarios(){
+      
+    ConnectionDB cn = new ConnectionDB();
+    Connection conexion = cn.Conectar();
+     
+     String [] nombresColumnas ={"DNI","Nombre", "Apellido", "Correo"}; 
+     DefaultTableModel modelo = new DefaultTableModel(null, nombresColumnas); 
+     String sql = "select * from usuario" ; 
+      
+      Statement st;
+      
+      TablaUsuarios.setModel(modelo);
+      String [] registros = new String[3];
+       
+      try {
+          st = conexion.createStatement();
+      
+          ResultSet result = st.executeQuery(sql);
+      
+          while(result.next()){
+              
+              System.out.println(result.getString(1));  
+          }
+          
+    }catch (SQLException e){
+        e.printStackTrace();
+    }
+
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton GuardarUsuario;
+    private javax.swing.JButton Limpiarcampo;
+    private javax.swing.JTable TablaUsuarios;
     private javax.swing.JButton btnDesactivar;
-    private javax.swing.JButton btnGuardar;
-    private javax.swing.JButton btnLimpiar;
     private javax.swing.JButton btnModificar;
     private javax.swing.JComboBox<String> cbHorario;
     private javax.swing.JComboBox<String> cbxActivo;
@@ -300,7 +349,6 @@ public class GestionDeUsuarios extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable tblUsuarios;
     private javax.swing.JTextField txtApellido;
     private javax.swing.JTextField txtContrasenia;
     private javax.swing.JTextField txtCorreo;
